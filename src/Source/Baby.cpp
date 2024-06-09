a78beeff47693bc3b9f04c20c70f89ac26bb172f
unknown <cedricba.gautier@gmail.com>
2024-06-09 19:59:21 +0200

    Ajout d'un équilibrage pour le multiplicateur de dégats que le bonheur du bébé donne

--- src/Source/Baby.cpp
+++ src/Source/Baby.cpp
@@ -6,7 +6,50 @@ Baby::Baby() {
 
 float Baby::GetMult() const
 {
-	return 0.0f;
+	if (m_happinessLvl < 0) {
+		//Erreur?
+		return 0.0f;
+	}
+	else if (m_happinessLvl<10)
+	{
+		return 0.25f;
+	}
+	else if (m_happinessLvl < 20)
+	{
+		return 0.5f;
+	}
+	else if (m_happinessLvl < 30)
+	{
+		return 1.0f;
+	}
+	else if (m_happinessLvl< 40)
+	{
+		return 1.25f;
+	}
+	else if (m_happinessLvl< 50)
+	{
+		return 1.5f;
+	}
+	else if (m_happinessLvl < 60)
+	{
+		return 1.75f;
+	}
+	else if (m_happinessLvl < 70)
+	{
+		return 2.0f;
+	}
+	else if (m_happinessLvl < 80)
+	{
+		return 2.25f;
+	}
+	else if (m_happinessLvl < 90)
+	{
+		return 2.5f;
+	}
+	else 
+	{
+		return 3.0f;
+	}
 }
 
 void Baby::Modify(int modifier)
