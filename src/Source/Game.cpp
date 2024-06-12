8ea2d1b531520b7f3b4fc37ebdd881c544ed2c82
unknown <cedricba.gautier@gmail.com>
2024-06-12 13:41:09 +0200

    Ajout de sprites des boutons

--- src/Source/Game.cpp
+++ src/Source/Game.cpp
@@ -28,21 +28,27 @@ void Game::Run() {
 	// définition du menu dans un premier temps
 	// définition du background du menu
 	sf::RectangleShape background_menu(static_cast<sf::Vector2f> ((*m_window).getSize()));
-	sf::Texture texture;
-	texture.loadFromFile("resources/Sprites/P.png");
-	background_menu.setTexture(&texture);
+	sf::Texture texture_background;
+	texture_background.loadFromFile("resources/Sprites/P.png");
+	background_menu.setTexture(&texture_background);
 	// définition du bouton "play"
 	sf::RectangleShape play_button(sf::Vector2f (200,100));
 	play_button.setPosition(sf::Vector2f(300, 50));
-	play_button.setTexture(&texture);
+	sf::Texture texture_play_button;
+	texture_play_button.loadFromFile("resources/Sprites/play_button.png");
+	play_button.setTexture(&texture_play_button);
 	// définition du bouton "rules"
 	sf::RectangleShape rules_button(sf::Vector2f(200, 100));
 	rules_button.setPosition(sf::Vector2f(300, 250));
-	rules_button.setTexture(&texture);
+	sf::Texture texture_rules_button;
+	texture_rules_button.loadFromFile("resources/Sprites/rules_button.png");
+	rules_button.setTexture(&texture_rules_button);
 	// définition du bouton "quit game"
 	sf::RectangleShape quit_button(sf::Vector2f(200, 100));
 	quit_button.setPosition(sf::Vector2f(300, 450));
-	quit_button.setTexture(&texture);
+	sf::Texture texture_quit_button;
+	texture_quit_button.loadFromFile("resources/Sprites/quit_button.png");
+	quit_button.setTexture(&texture_quit_button);
 	// affichage du menu
 	(*m_window).clear();
 	(*m_window).draw(background_menu);
