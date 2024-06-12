4516dc6a1dbc6bfc197136cf3606d871ee345af5
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-06-12 22:24:05 +0200

    Drawable Ready

--- src/Include/IDrawable.h
+++ src/Include/IDrawable.h
@@ -7,13 +7,17 @@ class IDrawable {
 
 public:
 	IDrawable() = default;
-	IDrawable(const sf::Vector2f& position, const sf::Texture& texture);
-	IDrawable(const sf::Vector2f& position, const std::string& spritePath);
+	IDrawable(const sf::Vector2f& position, const sf::Texture& texture, sf::Vector2i dimensions = { 1, 0 });
+	IDrawable(const sf::Vector2f& position, const std::string& spritePath, sf::Vector2i dimensions = { 1, 0 });
 
 	virtual ~IDrawable() = default;
 
 
 	virtual void Draw(sf::RenderWindow&) const;
+	
+
+	void SetDimensions(const sf::Vector2i& dimensions);
+	void SwtichSprite(int xIndex, int yIndex = 0);
 
 	// Getters
 	sf::Sprite& GetSprite();
@@ -23,4 +27,9 @@ public:
 private:
 	sf::Texture m_texture;
 	sf::Sprite m_sprite;
+
+	sf::Vector2i m_dimensions;
+
+	int m_xIndex = 0;
+	int m_yIndex = 0;
 };
--- src/Source/IDrawable.cpp
+++ src/Source/IDrawable.cpp
@@ -1,23 +1,54 @@
 #include "IDrawable.h"
 
-IDrawable::IDrawable(const sf::Vector2f& position, const sf::Texture& texture) :
+#include <iostream>
+
+IDrawable::IDrawable(const sf::Vector2f& position, const sf::Texture& texture, sf::Vector2i dimensions) :
 	m_texture(texture),
 	m_sprite(texture)
 {
 	m_sprite.setPosition(position);
+	SetDimensions(dimensions);
 }
 
-IDrawable::IDrawable(const sf::Vector2f& position, const std::string& spritePath)
+IDrawable::IDrawable(const sf::Vector2f& position, const std::string& spritePath, sf::Vector2i dimensions)
 {
 	sf::Texture texture;
 	texture.loadFromFile(spritePath);
 	m_sprite.setTexture(texture);
 	m_sprite.setPosition(position);
+	SetDimensions(dimensions);
 }
 
 void IDrawable::Draw(sf::RenderWindow& renderWindow) const
 {
 	renderWindow.draw(m_sprite);
+}
+
+void IDrawable::SetDimensions(const sf::Vector2i& dimensions)
+{
+	if (dimensions.x < 0 || dimensions.y < 0 || (dimensions.x == 0 && dimensions.y == 0))
+	{
+		std::cout << "Warning: Dimensions cannot be negative or both equal to zero. Setting to { 1, 0 }" << std::endl;
+
+		m_dimensions = { 1, 0 };
+
+		return;
+	}
+
+	m_dimensions = dimensions;
+}
+
+void IDrawable::SwtichSprite(int xIndex, int yIndex)
+{
+	m_xIndex = xIndex;
+	m_yIndex = yIndex;
+
+	m_sprite.setTextureRect(sf::IntRect(
+		m_xIndex / m_dimensions.x * m_texture.getSize().x,
+		m_yIndex / m_dimensions.y * m_texture.getSize().y,
+		(m_xIndex+1) / m_dimensions.x * m_texture.getSize().x,
+		(m_xIndex+1) / m_dimensions.x * m_texture.getSize().y
+	));
 }
 
 sf::Sprite& IDrawable::GetSprite()
