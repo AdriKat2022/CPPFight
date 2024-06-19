779916ec60e918d9ba9ae40bb05ed54d3462836b
AdriKat2022 <adrien.schroedel91@gmail.com>
2024-06-20 01:53:11 +0200

    replaced music

--- src/Source/GameRun.cpp
+++ src/Source/GameRun.cpp
@@ -15,7 +15,7 @@ GameRun::GameRun(sf::RenderWindow& rWindow) :
 
 	// Run music
 	m_currentMusic.openFromFile(FilePaths::MUSIC_RUN_MAIN);
-	m_currentMusic.setVolume(25*Config::GLOBAL_VOLUME_MULT);
+	m_currentMusic.setVolume(75*Config::GLOBAL_VOLUME_MULT);
 	m_currentMusic.play();
 	m_currentMusic.setLoop(true);
 
